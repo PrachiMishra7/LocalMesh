0000726da6db4d5978aec525ea0b9f7de53e5bab
PrachiMishra7 <prachimishralive@gmail.com>
2026-07-13 15:13:43 +0530

    fix express route bug

--- signalling-server/index.js
+++ signalling-server/index.js
@@ -11,7 +11,7 @@ const frontendPath = path.join(__dirname, '../dist');
 app.use(express.static(frontendPath));
 
 // Fallback to index.html for React Router / SPA navigation
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
